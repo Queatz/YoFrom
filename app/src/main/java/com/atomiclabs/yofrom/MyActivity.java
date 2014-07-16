14f04c6f140c9fa552e840033adb856c3c676afd
Jacob Ferrero <jacobaferrero@gmail.com>
2014-07-16 11:29:27 -0700

    Bugfix

--- app/src/main/java/com/atomiclabs/yofrom/MyActivity.java
+++ app/src/main/java/com/atomiclabs/yofrom/MyActivity.java
@@ -22,11 +22,6 @@ public class MyActivity extends Activity {
 
         setContentView(R.layout.activity_my);
 
-        Parse.initialize(this, "39R5Z3p67UaBZPFBbaolrE232Qvurmp0bUTTq4uY", "7baBEqEVXikWSKfzRf05RoRF2q9vfBSusWjt5vM5");
-        ParseObject.registerSubclass(User.class);
-        PushService.setDefaultPushCallback(getApplicationContext(), MyActivity.class);
-        ParseInstallation.getCurrentInstallation().saveInBackground();
-
         new App(this).start();
     }
 
